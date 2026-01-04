50169fc6785c245bd5b0c32f494bef58752fcdbc
WANDERLEY COSTA ALVES <wander.alves.dev@gmail.com>
2026-01-04 00:29:03 -0300

    Delete Projeto gerador de nome de banda.py

--- Projeto gerador de nome de banda.py
+++
@@ -1,8 +0,0 @@
-#1 Desafio de 100 projetos
-
-print("Bem vindo ao gerador de nomes de banda\n")
-
-cidade = input("Digite o nome da cidade que você nasceu: \n")
-pet = input('Digite o nome do seu animal de estimação: \n')
-
-print("O nome da sua banda será: " + cidade + ' ' + pet)
